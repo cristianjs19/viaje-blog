4496dc14948493253bcc513b9bf9babf7570dfbd
Cristianjs19 <cristianjs19@gmail.com>
2017-07-21 10:17:21 -0300

    avances front-end y back-end cercano a lanzamiento
    avances varios en front-end. cambios mas significativos en back-end
    creación categorías y demás en posts y albums

--- album/urls.py
+++ album/urls.py
@@ -3,21 +3,21 @@ from album import views
 
 
 urlpatterns = (
-                       url(r'^$', views.base, name='base1'),
-                       url(r'^category/$', views.CategoryListView, name='category-list'),
-                       url(r'^category/(?P<pk>\d+)/$', views.subCategoryView,
-                           name='category-detail'),
-                       url(r'^subcategory/(?P<pk>\d+)/$', views.ChildSubCategoryView,
-                           name='subcategory'),
-                       url(r'^childsubcategory/(?P<pk>\d+)/$', views.galleryView,
-                           name='subcategory'),
-                       # url(r'^photo/$', views.PhotoListView.as_view(), name='photo-list'),
-                       url(r'^gallery/(?P<pk>\d+)/detail/$', views.galleryDetailView,
-                           name='photo-detail'),
-                       # # Update
-                       # url(r'^photo/(?P<pk>\d+)/update/$', views.PhotoUpdate.as_view(), name='photo-update'),
-                       # #Create
-                       # url(r'^photo/create/$', views.PhotoCreate.as_view(), name='photo-create'),
-                       # #Delete
-                       # url(r'^photo/(?P<pk>\d+)/delete/$', views.PhotoDelete.as_view(), name='photo-delete'),
-                       )
+   url(r'^$', views.base, name='base1'),
+   url(r'^category/$', views.CategoryListView, name='category-list'),
+   url(r'^category/(?P<pk>\d+)/$', views.subCategoryView,
+       name='category-detail'),
+   url(r'^subcategory/(?P<pk>\d+)/$', views.ChildSubCategoryView,
+       name='subcategory'),
+   url(r'^childsubcategory/(?P<pk>\d+)/$', views.galleryView,
+       name='subcategory'),
+   # url(r'^photo/$', views.PhotoListView.as_view(), name='photo-list'),
+   url(r'^gallery/(?P<pk>\d+)/detail/$', views.galleryDetailView,
+       name='photo-detail'),
+   # # Update
+   # url(r'^photo/(?P<pk>\d+)/update/$', views.PhotoUpdate.as_view(), name='photo-update'),
+   # #Create
+   # url(r'^photo/create/$', views.PhotoCreate.as_view(), name='photo-create'),
+   # #Delete
+   # url(r'^photo/(?P<pk>\d+)/delete/$', views.PhotoDelete.as_view(), name='photo-delete'),
+   )
